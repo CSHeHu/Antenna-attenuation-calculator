0bca51662526026538584b6cb1fdd5fe75e18c3b
HeHu <heissendo88@gmail.com>
2023-11-29 21:24:06 +0200

    Added buttons and labels to layout. Added calculations to low annuations

--- Gui.py
+++ Gui.py
@@ -1,60 +1,95 @@
 import customtkinter as tk
 
-START_ROW = 1
+
 
 class Gui:
     def __init__(self, components):
         self.__components = components
-        self.__pcs_entries = []
+        self.__pcs_entries = [] # Store the entry fields
+        self.__pcs_amounts = [] # Stores the amounts of components to calculate
 
         tk.set_appearance_mode("system")
         tk.set_default_color_theme("blue")
 
         self.__root = tk.CTk()
-        self.__root.geometry("800x600")
+        self.__root.geometry("640x480")
 
         self.__root.title("Antenna attenuation calculator")
-        self.__root.grid_columnconfigure((0, 1, 2, 4, 5), weight=1)
+        self.__root.grid_columnconfigure((0, 1, 2, 3), weight=1)
 
-        self.printout_component_labels()
-        self.printout_components()
+        self.printout_components_frame()
         self.add_read_button()
         self.__root.mainloop()
 
+    def calculate_low_att(self):
+        total_low_att = 0
+        for index, component in enumerate(self.__components):
+            low_att = float(component.get_attenuation_low())
+            pcs = float(self.__pcs_amounts[index])
+            total_low_att = total_low_att + (pcs * low_att)
+        return total_low_att
+
+    def printout_components_frame(self):
+        px = 5
+        py = 5
+        self.__components_frame = tk.CTkFrame(self.__root)
+        self.__components_frame.grid_columnconfigure((0, 1, 2, 3), weight=1)
+        self.__components_frame.grid(row=0, column=0, padx=px, pady=py, sticky="nsew")
+
+        self.printout_component_labels()
+        self.printout_components()
 
     def printout_component_labels(self):
-        label01 = tk.CTkLabel(self.__root, text="Component",
+        px = 5
+        py = 5
+        label01 = tk.CTkLabel(self.__components_frame, text="Component",
                               fg_color="transparent")
-        label01.grid(row=START_ROW, column=0, padx=1, pady=20, sticky="ew")
-        label02 = tk.CTkLabel(self.__root, text="Attenuation, low",
+        label01.grid(row=0, column=0, padx=px, pady=py, sticky="nsew")
+        label02 = tk.CTkLabel(self.__components_frame, text="Attenuation, low",
                               fg_color="transparent")
-        label02.grid(row=START_ROW, column=1, padx=1, pady=20, sticky="ew")
-        label02 = tk.CTkLabel(self.__root, text="Attenuation, high",
+        label02.grid(row=0, column=1, padx=px, pady=py, sticky="nsew")
+        label02 = tk.CTkLabel(self.__components_frame, text="Attenuation, high",
                               fg_color="transparent")
-        label02.grid(row=START_ROW, column=2, padx=1, pady=20, sticky="ew")
+        label02.grid(row=0, column=2, padx=px, pady=py, sticky="nsew")
 
     def printout_components(self):
-        row = START_ROW + 1
-        for component in self.__components:
-            label1 = tk.CTkLabel(self.__root, text=component.get_type(), fg_color="transparent")
-            label1.grid(row=row, column=0, sticky="ew")
-            label2 = tk.CTkLabel(self.__root, text=component.get_attenuation_low(), fg_color="transparent")
-            label2.grid(row=row, column=1, sticky="ew")
-            label3 = tk.CTkLabel(self.__root, text=component.get_attenuation_high(), fg_color="transparent")
-            label3.grid(row=row, column=2,sticky="ew")
-            pcs_entry = tk.CTkEntry(self.__root, placeholder_text="pcs")
-            pcs_entry.grid(row=row, column=3, padx=3)
-            self.__pcs_entries.append(pcs_entry)  # Store the entry widgets
-            row += 1
+        px = 5
+        py = 5
+        for index, component in enumerate(self.__components, start=1):
+            label1 = tk.CTkLabel(self.__components_frame, text=component.get_type(), fg_color="transparent")
+            label1.grid(row=index, column=0, padx=px, sticky="nsew")
+            label2 = tk.CTkLabel(self.__components_frame, text=component.get_attenuation_low(), fg_color="transparent")
+            label2.grid(row=index, column=1, padx=px, sticky="nsew")
+            label3 = tk.CTkLabel(self.__components_frame, text=component.get_attenuation_high(), fg_color="transparent")
+            label3.grid(row=index, column=2, padx=px, sticky="nsew")
+            pcs_entry = tk.CTkEntry(self.__components_frame, placeholder_text="pcs")
+            pcs_entry.grid(row=index, column=3, padx=px, pady=py, sticky="nsew")
+            self.__pcs_entries.append(pcs_entry)  # Store the entry fields
+
 
     def add_read_button(self):
-        read_button = tk.CTkButton(self.__root, text="Read Entries", command=self.read_entries)
-        read_button.grid(row=6, column=1, padx=20, pady=20, sticky="ew", columnspan=1)
+        px = 5
+        py = 5
+        self.__read_frame = tk.CTkFrame(self.__root)
+        self.__read_frame.grid_columnconfigure((0, 1, 2, 3), weight=1)
+        self.__read_frame.grid(row=1, column=0, padx=px, pady=py, sticky="nsew")
+
+        read_button = tk.CTkButton(self.__read_frame, text="Read Entries", command=self.read_entries)
+        read_button.grid(row=0, column=3, padx=px, pady=py, sticky="nsew")
+        self.__sum_low_label = tk.CTkLabel(self.__read_frame, text="low sum", fg_color="transparent")
+        self.__sum_low_label.grid(row=0, column=1, padx=px, sticky="nsew")
+        self.__sum_high_label = tk.CTkLabel(self.__read_frame, text="high sum", fg_color="transparent")
+        self.__sum_high_label.grid(row=0, column=2, padx=px, sticky="nsew")
+
+
 
     def read_entries(self):
         for index, component in enumerate(self.__components):
             pcs_value = self.__pcs_entries[index].get()  # Get the value from the corresponding entry field
-            print(f"Component: {component.get_type()}, PCS value: {pcs_value}")
+            self.__pcs_amounts.append(pcs_value)
+
+        self.__sum_low_label.configure(text=f"{self.calculate_low_att():.1f}")
+        self.__pcs_amounts = [] # clear the list after calculations
 
 def main():
     ui = Gui()
