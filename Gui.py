fd446848b6424a8bf9d8406e476907eaaed0fec8
HeHu <heissendo88@gmail.com>
2023-11-30 14:08:37 +0200

    Added scrollable frame for components -window

--- Gui.py
+++ Gui.py
@@ -16,13 +16,12 @@ class Gui:
         tk.set_default_color_theme("blue")
 
         self.__root = tk.CTk()
-        self.__root.geometry("600x1200")
+        self.__root.geometry("600x700")
         self.__root.title("Antenna attenuation calculator")
         self.__root.grid_columnconfigure(0, weight=1)
         self.__root.grid_rowconfigure(0, weight=1)
 
-        self.printout_components_frame() # creates the main frame with
-        # components, their info etc
+        self.create_scrollable_frame()
         self.add_read_button_frame()
         self.add_sums_frame()
 
@@ -31,68 +30,33 @@ class Gui:
 
 
 
-    def calculate_low_att(self):
-        """
-        Calculates the total attenuations in low frequency
-        :return: float, total sum of low frequency attenuation
-        """
-        total_low_att = 0
-        for index, component in enumerate(self.__components):
-            try:
-                low_att = float(component.get_attenuation_low())
-                pcs = float(component.get_amount())
-                total_low_att = total_low_att + (pcs * low_att)
-            except ValueError:
-                total_low_att += 0
-        return total_low_att
+    def create_scrollable_frame(self):
+        self.__scrollable_frame = tk.CTkScrollableFrame(self.__root, width=550, height=600)
+        self.__scrollable_frame.grid(row=0, column=0, padx=20, pady=20)
 
-    def calculate_high_att(self):
-        """
-        Calculates the total attenuations in high frequency
-        :return: float, total sum of low frequency attenuation
-        """
-        total_high_att = 0
-        for index, component in enumerate(self.__components):
-            try:
-                high_att = float(component.get_attenuation_high())
-                pcs = float(component.get_amount())
-                total_high_att = total_high_att + (pcs * high_att)
-            except ValueError:
-                total_high_att += 0
-        return total_high_att
+        # Call the methods to print the component labels and components
+        self.printout_component_labels(self.__scrollable_frame)
+        self.printout_components(self.__scrollable_frame)
 
-    def printout_components_frame(self):
-        """
-        Creates the main components frame for GUI
-        """
-        px = 5
-        py = 5
-        self.__components_frame = tk.CTkFrame(self.__root)
-        self.__components_frame.grid_columnconfigure((0, 1, 2, 3), weight=1)
-        self.__components_frame.grid(row=0, column=0, padx=px,
-        pady=py, sticky="nsew")
 
-        self.printout_component_labels()
-        self.printout_components()
-
-    def printout_component_labels(self):
+    def printout_component_labels(self, parent_frame):
         """
         Creates the main labels for components from labels
         :return:
         """
         px = 5
         py = 5
-        label01 = tk.CTkLabel(self.__components_frame, text="Component",
+        label01 = tk.CTkLabel(parent_frame, text="Component",
                               fg_color="transparent")
         label01.grid(row=0, column=0, padx=px, pady=py, sticky="nsew")
-        label02 = tk.CTkLabel(self.__components_frame, text="Attenuation, low",
+        label02 = tk.CTkLabel(parent_frame, text="Attenuation, low",
                               fg_color="transparent")
         label02.grid(row=0, column=1, padx=px, pady=py, sticky="nsew")
-        label02 = tk.CTkLabel(self.__components_frame, text="Attenuation, high",
+        label03 = tk.CTkLabel(parent_frame, text="Attenuation, high",
                               fg_color="transparent")
-        label02.grid(row=0, column=2, padx=px, pady=py, sticky="nsew")
+        label03.grid(row=0, column=2, padx=px, pady=py, sticky="nsew")
 
-    def printout_components(self):
+    def printout_components(self, parent_frame):
         """
         Goes trough the list of components and prints out their information.
         After each component a entry field will be created which are stored
@@ -101,19 +65,19 @@ class Gui:
         px = 5
         py = 5
         for index, component in enumerate(self.__components, start=1):
-            label1 = tk.CTkLabel(self.__components_frame,
+            label1 = tk.CTkLabel(parent_frame,
             text=component.get_type(), fg_color="transparent")
             label1.grid(row=index, column=0, padx=px, sticky="nsew")
 
-            label2 = tk.CTkLabel(self.__components_frame,
+            label2 = tk.CTkLabel(parent_frame,
             text=component.get_attenuation_low(), fg_color="transparent")
             label2.grid(row=index, column=1, padx=px, sticky="nsew")
 
-            label3 = tk.CTkLabel(self.__components_frame,
+            label3 = tk.CTkLabel(parent_frame,
             text=component.get_attenuation_high(), fg_color="transparent")
             label3.grid(row=index, column=2, padx=px, sticky="nsew")
 
-            pcs_entry = tk.CTkEntry(self.__components_frame, placeholder_text="pcs")
+            pcs_entry = tk.CTkEntry(parent_frame, placeholder_text="pcs")
             pcs_entry.grid(row=index, column=3, padx=px, pady=py, sticky="nsew")
             self.__pcs_entries.append(pcs_entry)  # Store the amounts
             # of components to list from entry fields
@@ -169,6 +133,35 @@ class Gui:
         self.__sum_high_label.configure(text=f"{self.calculate_high_att():.1f}")
         self.__pcs_amounts = [] # clear the list after calculations
 
+    def calculate_low_att(self):
+        """
+        Calculates the total attenuations in low frequency
+        :return: float, total sum of low frequency attenuation
+        """
+        total_low_att = 0
+        for index, component in enumerate(self.__components):
+            try:
+                low_att = float(component.get_attenuation_low())
+                pcs = float(component.get_amount())
+                total_low_att = total_low_att + (pcs * low_att)
+            except ValueError:
+                total_low_att += 0
+        return total_low_att
+
+    def calculate_high_att(self):
+        """
+        Calculates the total attenuations in high frequency
+        :return: float, total sum of low frequency attenuation
+        """
+        total_high_att = 0
+        for index, component in enumerate(self.__components):
+            try:
+                high_att = float(component.get_attenuation_high())
+                pcs = float(component.get_amount())
+                total_high_att = total_high_att + (pcs * high_att)
+            except ValueError:
+                total_high_att += 0
+        return total_high_att
 
 def main():
     ...
